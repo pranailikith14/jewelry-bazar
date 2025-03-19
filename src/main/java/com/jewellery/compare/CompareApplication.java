e81c17d2388d54e38af0052e4132dcb1b2143d77
pranailikith14 <pranaylikith06@gmail.com>
2025-03-20 00:06:16 +0530

    Initial commit of Jewelry Bazar

---
+++ src/main/java/com/jewellery/compare/CompareApplication.java
@@ -0,0 +1,13 @@
+package com.jewellery.compare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareApplication.class, args);
+	}
+
+}
