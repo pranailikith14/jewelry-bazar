946df40f3966a47562663bd9b52eb1e20d98a73e
Pranathi-A <pranathi.addaganti@partanalytics.com>
2025-05-18 23:47:34 +0530

    "CORS"

--- backend/jewelrystore_microservice/src/main/java/com/jewelry/jewelrystore_microservice/JewelrystoreMicroserviceApplication.java
+++ backend/jewelrystore_microservice/src/main/java/com/jewelry/jewelrystore_microservice/JewelrystoreMicroserviceApplication.java
@@ -3,8 +3,10 @@ package com.jewelry.jewelrystore_microservice;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class JewelrystoreMicroserviceApplication {
 
 	public static void main(String[] args) {
---
+++ backend/jewelrystore_microservice/src/main/java/com/jewelry/jewelrystore_microservice/configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.jewelry.jewelrystore_microservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("http://localhost:4200") // Your Angular app's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
